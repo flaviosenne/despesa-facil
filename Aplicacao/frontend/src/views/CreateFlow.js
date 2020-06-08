cbad61c6aaabc7139b8b0de7ea001bf8d6eae386
flaviosenne <flaviosenne123@gmail.com>
2020-06-08 07:33:14 -0300

    Aleterei os estilos dos links

--- Aplicacao/frontend/src/views/CreateFlow.js
+++ Aplicacao/frontend/src/views/CreateFlow.js
@@ -4,43 +4,43 @@ import '../CSS/CreateUser.css';
 import { Link } from 'react-router-dom'
 import dinheiro from '../icons/dinheiro.png'
 import icon from '../icons/cash+.png'
-const props = {icon, route: '/fluxo-caixa'}
+const props = { icon, route: '/fluxo-caixa' }
 export default User => {
-return (
-    <>
-    <Header {...props}/>
-    <div className = 'usuario login'>
-        <img  className = "dinheiro"src = {dinheiro} alt = "icone dinheiro"/>
-        <label> Data </label>
-        <input  type = "date"/>
+    return (
+        <>
+            <Header {...props} />
+            <div className='usuario login'>
+                <img className="dinheiro" src={dinheiro} alt="icone dinheiro" />
+                <label> Data </label>
+                <input type="date" />
 
-        <label> Tipo </label>
-    <div className = "tipo">
+                <label> Tipo </label>
+                <div className="tipo">
 
-        <input type ="radio" name = "tipo" /> <label >Despesa</label>
-    </div>
-    <div className = "tipo">
-        <input type ="radio" name= "tipo"/> <label >Receita </label>
-    </div>
+                    <input type="radio" name="tipo" /> <label >Despesa</label>
+                </div>
+                <div className="tipo">
+                    <input type="radio" name="tipo" /> <label >Receita </label>
+                </div>
 
-        <label> Descrição </label>
-        <input />
+                <label> Descrição </label>
+                <input />
 
-        <label> Status </label>
-    <div className = "tipo">
+                <label> Status </label>
+                <div className="tipo">
 
-        <input type ="radio" name = "tipo" /> <label >Pendente</label>
-    </div>
-    <div className = "tipo">
-        <input type ="radio" name= "tipo"/> <label >Finalizado </label>
-    </div>
+                    <input type="radio" name="status" /> <label >Pendente</label>
+                </div>
+                <div className="tipo">
+                    <input type="radio" name="status" /> <label >Finalizado </label>
+                </div>
 
-        <label> Valor </label>
-        <input type = "number"/>
+                <label> Valor </label>
+                <input type="number" />
 
-       <Link to = "/fluxo-caixa"><button type = 'submit' > Cadastrar </button></Link>
-       <Link to = "/fluxo-caixa" ><button > Cancelar </button></Link>
-    </div>
-    </>
-   )
+                <Link to="/fluxo-caixa"><button type='submit' > Cadastrar </button></Link>
+                <Link to="/fluxo-caixa" ><button > Cancelar </button></Link>
+            </div>
+        </>
+    )
 }
--- Aplicacao/frontend/src/views/CreateUser.js
+++ Aplicacao/frontend/src/views/CreateUser.js
@@ -4,31 +4,31 @@ import '../CSS/CreateUser.css';
 import { Link } from 'react-router-dom'
 import icon from '../icons/user+.png'
 
-const props = {icon, route: '/login'}
+const props = { icon, route: '/login' }
 export default User => {
-return (
-    <>
-    <Header {...props}/>
-    <div className = 'usuario login'>
-        <img  src = {icon} alt = "icone usuario"/>
-        <label> Nome </label>
-        <input />
+    return (
+        <>
+            <Header {...props} />
+            <div className='usuario login'>
+                <img src={icon} alt="icone usuario" />
+                <label> Nome </label>
+                <input />
 
-        <label> Email </label>
-        <input />
+                <label> Email </label>
+                <input />
 
-        <label> Usuario </label>
-        <input />
+                <label> Usuario </label>
+                <input />
 
-        <label> Senha </label>
-        <input type = "password"/>
+                <label> Senha </label>
+                <input type="password" />
 
-        <label> Confirmar Senha </label>
-        <input type = "password"/>
+                <label> Confirmar Senha </label>
+                <input type="password" />
 
-       <Link to = "/home"><button type = 'submit' > Cadastrar </button></Link>
-       <Link to = "/" ><button type = 'submit' > Cancelar </button></Link>
-    </div>
-    </>
-   )
+                <Link to="/home"><button type='submit' > Cadastrar </button></Link>
+                <Link to="/" ><button type='submit' > Cancelar </button></Link>
+            </div>
+        </>
+    )
 }
