0ccf959562f9088629fba9442d92cf88c90222a1
Joao flavio <flaviosenne123@gmail.com>
2021-06-16 10:14:38 -0300

    query select default implement

--- Aplicação/server-ts/src/repositories/PostingsRepository.ts
+++ Aplicação/server-ts/src/repositories/PostingsRepository.ts
@@ -3,12 +3,21 @@ import { EntityRepository, Repository } from "typeorm";
 
 @EntityRepository(Postings)
 export class PostingsRepository extends Repository<Postings>{
-    filterByDate = (dateStart: string, dateEnd: string ): Promise<Postings| undefined> =>{
-        return this.query(`
-        select * from postings p 
-        inner join category c
-        inner join status s
-        inner join user u
-        where p.postings_date between $1 and $2`, [dateStart, dateEnd])
+    filterByDate = (dateStart: string, dateEnd: string ) =>{
+        // return this.query(`
+        // select * from postings p 
+        // inner join category c
+        // inner join status s
+        // inner join user u
+        // where p.postings_date between 1 and 2`, [dateStart, dateEnd])
+        return this.createQueryBuilder()
+        .select('p')
+        .from(Postings, 'p')
+        .innerJoinAndSelect('p.status', 's')
+        .innerJoinAndSelect('p.type', 't')
+        .innerJoin('p.user', 'u')
+        .where('p.postings_date between :dateStart and :dateEnd')
+        .setParameters({dateStart, dateEnd})
+        .getMany()
     }
 }
